09ac2c1ad4f3128ca79b1da36095e25d7e1c4ec3
lukasbash <jorglukasbash@gmail.com>
2024-06-26 07:36:06 +0200

    Added numeric key sorting to key-traversals.js

--- src/util/key-traversals.js
+++ src/util/key-traversals.js
@@ -1,13 +1,29 @@
 // case sensitive traversal orders
 const keyTraversals = {
-  asc: obj => Object.keys(obj).sort(),
-  desc: obj => Object.keys(obj).sort((a, b) => {
-    // note, objects can't have duplicate keys of the same case
-    if (a < b) {
-      return 1;
-    }
-    return -1;
-  })
+  asc: (obj) =>
+    Object.keys(obj).sort((a, b) => {
+      const aComp = isNaN(a) ? a : parseInt(a);
+      const bComp = isNaN(b) ? b : parseInt(b);
+
+      // note, objects can't have duplicate keys of the same case
+      if (aComp > bComp) {
+        return 1;
+      }
+
+      return -1;
+    }),
+  desc: (obj) =>
+    Object.keys(obj).sort((a, b) => {
+      const aComp = isNaN(a) ? a : parseInt(a);
+      const bComp = isNaN(b) ? b : parseInt(b);
+
+      // note, objects can't have duplicate keys of the same case
+      if (aComp < bComp) {
+        return 1;
+      }
+
+      return -1;
+    }),
 };
 
 module.exports = keyTraversals;
